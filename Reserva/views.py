a36d7af315f88a5b8c811eb2f39c0330e22b63b3
gonzandre23 <gonza.andre2@gmail.com>
2018-11-11 18:14:03 -0300

    menu superior

--- Reserva/views.py
+++ Reserva/views.py
@@ -10,15 +10,63 @@ from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 
 # Create your views here.
+
+def user_cliente(usuario,cliente):
+    if(cliente):
+        try:
+            Cliente.objects.get(usuario=usuario)
+            return False
+        except Exception as e:
+            return True
+    else:
+        try:
+            Cliente.objects.get(usuario=usuario)
+            return True
+        except Exception as e:
+            return False
+
+def user_ejecutivo(user,ejecutivo):
+    if(ejecutivo):
+        try:
+            Ejecutivo.objects.get(usuario=usuario)
+            return False
+        except Exception as e:
+            return True
+    else:
+        try:
+            Ejecutivo.objects.get(usuario=usuario)
+            return True
+        except Exception as e:
+            return False
+
 @login_required(login_url='/auth/login')
 def index(request):
-	data={}
-	template_name = "index_super_user.html"
-	return render(request, template_name,data)
+    data={}
+    data['inicio']=0
+    if (user_cliente(request.user,False)):
+        data['inicio']= 1
+    elif (user_ejecutivo(request.user,False)):
+        data['inicio']= 2
+    elif (request.user.is_staff == True):
+        data['inicio'] = 3
+    
+    data["request"] = request
+    template_name = "index_super_user.html"
+    return render(request, template_name,data)
 
 @login_required(login_url='/auth/login')
 def reservar_bici(request):
-	data={}
-	template_name = "reserva.html"
-	data["bicicletas"]=Bicicleta.objects.all()
-	return render(request, template_name,data)
+    data={}
+    data['inicio'] = 0
+    
+    if (user_cliente(request.user,False)):
+        data['inicio']= 1
+    elif (user_ejecutivo(request.user,False)):
+        data['inicio']= 2
+    elif (request.user.is_staff == True):
+        data['inicio'] = 3
+        
+    data["request"] = request
+    template_name = "reserva.html"
+    data["bicicletas"]=Bicicleta.objects.all()
+    return render(request, template_name,data)
