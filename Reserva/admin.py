5ccc6f9e43e1ac9503c18c98ca9f0a018c2256ca
daniel <danielmontecinoszambra@gmail.com>
2018-11-13 17:21:26 -0300

    probando cookie normal, funcionando

--- Reserva/admin.py
+++ Reserva/admin.py
@@ -10,7 +10,23 @@ class BicicletaAdmin(admin.ModelAdmin):
 @admin.register(Cliente)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('Nombre','Fecha_nacimiento','Email','Direccion',)
-    
+
     def thumb(self, obj):
         return mark_safe(u'<img src="%s" style="width:10px;height:10px;"/>' \
             % (obj.picture.url))
+
+@admin.register(Sucursal)
+class SucursalAdmin(admin.ModelAdmin):
+    list_display = ('Nombre','Direccion',)
+
+@admin.register(Tarjeta_credito)
+class Tarjeta_creditoAdmin(admin.ModelAdmin):
+    list_display = ('Nombre','Numero_tarjeta_redito','Fecha_vencimiento',)
+
+@admin.register(Reserva)
+class Reserva(admin.ModelAdmin):
+    list_display = ('Fecha_arriendo_inicial','Hora_arriendo_inicial','Fecha_arriendo_final','Hora_arriendo_final','Sucursal_inicio','Sucursal_fin',)
+
+@admin.register(Detalles_reserva)
+class Detalles_reservaAdmin(admin.ModelAdmin):
+    list_display = ('Cantidad','Bicicleta','Reserva',)
