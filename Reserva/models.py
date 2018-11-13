4797df0def4fb2791dcbebe8f4d7ef641626ce9d
daniel <danielmontecinoszambra@gmail.com>
2018-11-13 16:23:03 -0300

    migraciones y modelos nuevos

--- Reserva/models.py
+++ Reserva/models.py
@@ -24,5 +24,24 @@ class Cliente(models.Model):
     rut = models.CharField(max_length=8)
     dv = models.PositiveIntegerField()
 
+class Sucursal(models.Model):
+    Nombre = models.CharField(max_length=100)
+    Direccion = models.CharField(max_length=100)
+
+class Tarjeta_credito(models.Model):
+    Nombre = models.CharField(max_length=25)
+    Numero_tarjeta_redito = models.CharField(max_length=19)
+    Fecha_vencimiento = models.DateField()
+
 class Reserva(models.Model):
-    pass
+    Fecha_arriendo_inicial = models.DateField()
+    Hora_arriendo_inicial = models.TimeField()
+    Fecha_arriendo_final = models.DateField()
+    Hora_arriendo_final = models.TimeField()
+    Sucursal_inicio = models.ForeignKey(Sucursal, on_delete=models.CASCADE,related_name='%(class)s_inicio')
+    Sucursal_fin = models.ForeignKey(Sucursal, on_delete=models.CASCADE,related_name='%(class)s_fin')
+
+class Detalles_reserva(models.Model):
+    Cantidad = models.PositiveIntegerField()
+    Bicicleta = models.ForeignKey(Bicicleta, on_delete=models.CASCADE)
+    Reserva = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
