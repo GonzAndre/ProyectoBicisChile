df2ac8bd15d717de991d86e8f58dbdd67366d2d8
daniel <danielmontecinoszambra@gmail.com>
2018-11-16 20:30:45 -0300

    dejando formato de hora y fecha entrega y retiro listo

--- Reserva/views.py
+++ Reserva/views.py
@@ -31,11 +31,71 @@ def detalle_reserva(request):
 
 	sucursales = Sucursal.objects.all()
 	data['sucursales'] = sucursales
-	print (data)
+	# print (data)
+	# print("user: " + str(request.user.pk))
 	if (request.POST):
 		bicis_stock = request.POST['bicis']
 		fecha_retiro = request.POST['fecha_retiro']
-		fecha_entrega = request.POST['fecha_retiro']
+		cont = 0
+		fecha_retiro_final=""
+		hora_retiro_final=""
+		#haciendo formato de bd retiro
+		for i in fecha_retiro.split(","):
+			print("fecha retiro: " + i)
+			if (cont==0):
+				fecha_retiro_final = fecha_retiro_final + i +"-"
+				cont = cont +1
+			elif (cont == 1):
+				fecha_retiro_final = fecha_retiro_final + i +"-"
+				cont = cont +1
+			else:
+				aux_else = 0
+				for n in i.split("T"):
+					if (aux_else == 0):
+						fecha_retiro_final = fecha_retiro_final + n
+						aux_else = aux_else + 1
+					else:
+						hora_retiro_final = hora_retiro_final + n
+		print("fecha retiro final: " +fecha_retiro_final)
+		print("hora retiro final: " +hora_retiro_final)
+
+
+
+
+		fecha_entrega = request.POST['fecha_entrega']
+		cont = 0
+		fecha_entrega_final=""
+		hora_entrega_final=""
+		#haciendo formato de bd entrega
+		for i in fecha_entrega.split(","):
+			print("fecha entrega: " + i)
+			if (cont==0):
+				fecha_entrega_final = fecha_entrega_final + i +"-"
+				cont = cont +1
+			elif (cont == 1):
+				fecha_entrega_final = fecha_entrega_final + i +"-"
+				cont = cont +1
+			else:
+				aux_else = 0
+				for n in i.split("T"):
+					if (aux_else == 0):
+						fecha_entrega_final = fecha_entrega_final + n
+						aux_else = aux_else + 1
+					else:
+						hora_entrega_final = hora_entrega_final + n
+		print("fecha entrega final: " +fecha_entrega_final)
+		print("hora entrega final: " +hora_entrega_final)
+		# sucursal = Sucursal(request.user.pk)
+
+		# class Reserva(models.Model):
+		#     User = models.ForeignKey(User,on_delete=models.CASCADE,related_name='%(class)s_user')
+		#     Fecha_arriendo_inicial = models.DateField()
+		#     Hora_arriendo_inicial = models.TimeField()
+		#     Fecha_arriendo_final = models.DateField()
+		#     Hora_arriendo_final = models.TimeField()
+		#     Sucursal_inicio = models.ForeignKey(Sucursal, on_delete=models.CASCADE,related_name='%(class)s_inicio')
+		#     Sucursal_fin = models.ForeignKey(Sucursal, on_delete=models.CASCADE,related_name='%(class)s_fin')
+
 	# if request.POST:
 	# 	lista_obj = []
 	# 	filtro = []
